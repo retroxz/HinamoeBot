c2f992e67df0e466b622351591f1d9b74683e9b8
retroxz <zzxee666@gmail.com>
2020-12-29 16:57:25 +0800

    fix(rbtt): 修复熊兔贴贴插件Linux兼容性BUG

--- HinaMoe-plugin-rbtt/index.js
+++ HinaMoe-plugin-rbtt/index.js
@@ -1,18 +1,21 @@
 const {default: Bot} = require('el-bot')
 const { Message } = require('mirai-ts')
-const path = '.\\resource\\rbtt\\images'
+const path = require('path')
+const SEP = path.sep
+const imagePath = `.${SEP}${SEP}resource${SEP}${SEP}rbtt${SEP}${SEP}images${SEP}${SEP}`
 const fs = require('fs')
 
 module.exports = function (ctx) {
+
   const mirai = ctx.mirai
   mirai.on('GroupMessage',async msg => {
     const id = msg.sender.group.id
     if(ctx.el.config.rbtt.listen.indexOf(id) !==- 1){
 
       if(msg.plain === '熊兔贴贴'){
-        const images = fs.readdirSync(path)
+        const images = fs.readdirSync(imagePath)
         let fileName = images[Math.floor(Math.random() * images.length)]
-        let image = await mirai.api.uploadImage('group',`${path}\\${fileName}`)
+        let image = await mirai.api.uploadImage('group',`${imagePath}${fileName}`)
         await mirai.api.sendGroupMessage([Message.Image('', image.url,'')], id)
       }
     }
