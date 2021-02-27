8e3c1837566358d706f0e061c63660712d606b83
retroxz <zzxee666@gmail.com>
2021-02-27 16:45:03 +0800

    :recycle: 更新目录结构 1. 插件移动到plugins目录单独存放 2. bot本体加入项目

--- HinaMoe-plugin-bilibili/model/Subscribes.js
+++
@@ -1,9 +0,0 @@
-// 订阅表
-const Base = require('./Base')
-
-
-exports.add = async function (params) {
-  let sql = "INSERT INTO `bot_subscribes`( `qid`, `qnick`, `type`, `qtype`, `uid`, `room_id`, `nick_name`, `operator_id`, `operator_nick`, `create_time`) " +
-    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
-  return await Base.query(sql, [params.qid, params.qnick, params.type, params.qtype, params.uid, params.roomId, params.nick, params.operatorId, params.operatorNick, params.createTime])
-}
--- HinaMoe-plugin-bilibili/scope/SubscribeType.js
+++
@@ -1,6 +0,0 @@
-// 视频
-exports.VIDEO = 1
-// 动态
-exports.DYNAMIC = 2
-// 直播
-exports.LIVE = 3
