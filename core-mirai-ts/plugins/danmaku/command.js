c27432dda6951bd27f43f06ee962f49003404eb3
retroxz <zzxee666@gmail.com>
2021-03-09 21:39:36 +0800

    :sparkles: 弹幕库支持不同群配置不同弹幕库 添加【弹幕帮助】命令

--- core-mirai-ts/plugins/danmaku/command.js
+++ core-mirai-ts/plugins/danmaku/command.js
@@ -1,9 +1,10 @@
+const {default: Bot} = require('el-bot')
 const commandsMap = new Map()
 const Util = require('./utils')
 const DanmakuModel = require('./model/Danmaku')
 const BiliSearch = require('./api/search')
 const BiliSpace = require('./api/space')
-
+let roomId = 22603245
 
 exports.danmakuRank = async function (message) {
     const dateRegex = '(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])'
@@ -21,7 +22,7 @@ exports.danmakuRank = async function (message) {
                 throw new Error('日期格式不对呀')
             }
         }
-        const res = await DanmakuModel.danmakuRankByDate(currentDate)
+        const res = await DanmakuModel.danmakuRankByDate(currentDate, roomId)
         if (res.length === 0)
             throw new Error(`找不到${Util.getCurrentDateString(currentDate)}的弹幕记录！`)
         let replyMsg = `${Util.getCurrentDateString(currentDate)} 弹幕TOP10:\n`
@@ -40,33 +41,44 @@ exports.danmakuRandom = async function (message) {
         let uname = ''
         if (isNaN(parseInt(uid))) {
             const searchRes = await BiliSearch.searchUser(uid)
-            if(!searchRes.data.hasOwnProperty('result'))
+            if (!searchRes.data.hasOwnProperty('result'))
                 throw new Error(`找不到用户${uid}`)
             uname = uid
             uid = searchRes.data.result[0].mid
-        }else{
+        } else {
             const spaceRes = await BiliSpace.spaceInfo(uid)
-            if(spaceRes.code === -404)
+            if (spaceRes.code === -404)
                 throw new Error(`找不到用户${uid}`)
             uname = spaceRes.data.name
         }
         let replyMsg = `${uname}的弹幕语录:\n`
-        const res = await DanmakuModel.randomDanmakuByUser(uid)
-        if(res.length === 0)
+        const res = await DanmakuModel.randomDanmakuByUser(uid, roomId)
+        if (res.length === 0)
             throw new Error(`找不到${uname}的弹幕记录`)
-        for(let i = 0,len = res.length; i < len;i++){
+        for (let i = 0, len = res.length; i < len; i++) {
             replyMsg += `${res[i]['timestamp']}:${res[i]['msg']}\n`
         }
         message.reply(replyMsg)
-    }catch (e) {
+    } catch (e) {
         message.reply(e.message)
     }
 
 }
+exports.danmakuHelp = async function (message) {
+    let danmakuHelpMsg = `弹幕库功能\n1. 【弹幕排行】每日弹幕排行 后面跟两位数日期可以查询指定日期排行\n例如: 弹幕排行03-05\n2. 【弹幕语录XXX】随机显示XXX十条弹幕 可以填入昵称或UID\n注意: 每个群弹幕库不同 例如薯条相关群就是薯条弹幕库`
+    message.reply(danmakuHelpMsg)
+}
 commandsMap.set('弹幕排行', exports.danmakuRank)
 commandsMap.set('弹幕语录', exports.danmakuRandom)
+commandsMap.set('弹幕帮助', exports.danmakuHelp)
 
 exports.match = function (message) {
+    for (let i = 0, len = message.config.length; i < len; i++) {
+        if (message.config[i].group === message.sender.group.id) {
+            roomId = message.config[i].listen
+            break
+        }
+    }
     const plain = message.plain
     if (!plain) return
     for (let key of commandsMap.keys()) {
--- core-mirai-ts/plugins/danmaku/index.js
+++ core-mirai-ts/plugins/danmaku/index.js
@@ -4,6 +4,7 @@ const Command = require('./command')
 module.exports = function (ctx) {
     const mirai = ctx.mirai
     mirai.on('GroupMessage',message => {
+        message.config = ctx.el.bot.danmaku
         Command.match(message)
     })
 }
