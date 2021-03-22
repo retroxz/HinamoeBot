81228cad44a4eeb55efd8bc4b77e234d321734d7
retroxz <zzxee666@gmail.com>
2021-03-23 00:47:53 +0800

    :sparkles: 新增早晚问候插件

---
+++ core-mirai-ts/plugins/greeting/model/Base.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql')
+const pool = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.BOT_DATABASE
+})
+
+exports.query = function (sql, params) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            conn.query(sql, params, (err, rows) => {
+                if (err) {
+                    reject(err)
+                    conn.release()
+                } else {
+                    resolve(rows)
+                    conn.release()
+                }
+            })
+
+        })
+    })
+}
