8c1cd16493f8a891cb6db3e87e9999b5fd1cbbeb
retroxz <zzxee666@gmail.com>
2021-05-06 10:13:57 +0800

    :memo: 新增requirements.txt

--- core-nonebot/bot.py
+++ core-nonebot/bot.py
@@ -6,7 +6,6 @@ import nonebot
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 from utils.plugin_data import PLUGINS_DATA_DIR
 
-
 # 初始化
 nonebot.init()
 # 加载协议
@@ -14,16 +13,12 @@ driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)  # 使用go-cqhttp
 
 # 加载插件
-# nonebot.load_plugin("plugins.spade_echo")
 nonebot.load_plugin("plugins.mute")
-
-
-
 
 if __name__ == "__main__":
     # 初始化配置文件夹
     if not os.path.exists(PLUGINS_DATA_DIR):
         os.makedirs(PLUGINS_DATA_DIR)
     app = nonebot.get_asgi()
-    nonebot.run(app="__mp_main__:app")
-    # nonebot.run()
+    # nonebot.run(app="__mp_main__:app")
+    nonebot.run()
