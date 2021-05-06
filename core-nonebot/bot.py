43e30b0d9fdfe23e0c3f1624498e56c998d188a5
retroxz <zzxee666@gmail.com>
2021-05-06 14:35:11 +0800

    :bug: 修复ASGI启动错误

--- core-nonebot/bot.py
+++ core-nonebot/bot.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-
+from utils.plugin_data import PLUGINS_DATA_DIR
 import nonebot
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
-from utils.plugin_data import PLUGINS_DATA_DIR
 
-# 初始化
+
 nonebot.init()
-# 加载协议
+app = nonebot.get_asgi()
+
 driver = nonebot.get_driver()
-driver.register_adapter("cqhttp", CQHTTPBot)  # 使用go-cqhttp
+driver.register_adapter("cqhttp", CQHTTPBot)
 
 # 加载插件
 nonebot.load_plugin("plugins.mute")
@@ -19,6 +19,4 @@ if __name__ == "__main__":
     # 初始化配置文件夹
     if not os.path.exists(PLUGINS_DATA_DIR):
         os.makedirs(PLUGINS_DATA_DIR)
-    app = nonebot.get_asgi()
-    # nonebot.run(app="__mp_main__:app")
-    nonebot.run()
+    nonebot.run(app="__mp_main__:app")
