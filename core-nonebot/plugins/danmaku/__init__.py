79c2b3fe408e54c3b274d78cbe9b9c4e0be3f55f
retroxz <zzxee666@gmail.com>
2021-09-02 17:03:20 +0800

    :bug: 修正查询弹幕时间解析错误的bug

--- core-nonebot/plugins/danmaku/__init__.py
+++ core-nonebot/plugins/danmaku/__init__.py
@@ -1,8 +1,11 @@
 # !/usr/bin/env python3
 # coding=utf-8
+import datetime
+
 import nonebot
 from nonebot import on_startswith
 from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
 from nonebot.adapters import Bot, Event
 from .model import *
 from utils import Plugin_Data
@@ -36,6 +39,15 @@ async def search_user(username):
         'search_type': 'bili_user'
     }).json()['data']
 
+
+async def validate_data(raws, date):
+    index = 0
+    for raw in raws:
+        if raw['timestamp'] > int(date.timestamp() * 1000):
+            break
+        index += 1
+    return raws[:index]
+
 query_danmaku = on_startswith('查询弹幕')
 
 
@@ -47,13 +59,18 @@ async def _(bot: Bot, event: Event):
     if len(params) < 2:
         await query_danmaku.finish(F"参数格式不正确呀\n例如: 查询弹幕 永雏塔菲 2021-08-01 2(可选)")
 
-    # 默认设置为本年日期
-    if len(params[1].split('-')) == 2:
-        params[1] = F"{datetime.now().year}-{params[1]}"
     # 验证日期格式
     query_date = None
     try:
+        # 转换日期格式
+        params[1].replace('.','')
+        params[1].replace('-','')
+        params[1].replace('/','')
+        if len(params[1]) == 4:
+            params[1] = F"{datetime.now().year}{params[1]}"
         query_date = parse(params[1])
+        if query_date.year < 2000:
+            raise Exception
     except:
         await query_danmaku.finish(F"日期格式不正确呀\n例如: 查询弹幕 永雏塔菲 2021-08-01 2")
 
@@ -68,12 +85,16 @@ async def _(bot: Bot, event: Event):
     # 查询数据
     page = 1 if len(params) == 2 else params[2]
     danmaku_list = await query_danmaku_list(DEFAULT_ROOM_ID, query_date.strftime('%Y-%m-%d'), uid, page)
+    # 过滤数据
+    query_date = query_date + relativedelta(days=1)
+    danmaku_list = await validate_data(danmaku_list, query_date)
+    query_date = query_date - relativedelta(days=1)
     if not len(danmaku_list):
-        await query_danmaku.finish(F"找不到用户[{username}]的弹幕数据")
+        await query_danmaku.finish(F"找不到{query_date.strftime('%Y年%m月%d日')}用户[{username}]的弹幕数据")
 
     # 构造返回
     index = 1
-    reply_message = F"{query_date.strftime('%Y年%m月%d日')} {username}的弹幕数据:\n"
+    reply_message = F"{query_date.strftime('%Y年%m月%d日')}{username}的弹幕数据:\n"
     for danmaku in danmaku_list:
         reply_message += F"{index}. {danmaku['msg']}\n"
         index += 1
