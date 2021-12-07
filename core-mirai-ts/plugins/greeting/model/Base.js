62226f69138eae9282b04b973537b5a5a6d0a9c8
retroxz <zzxee666@gmail.com>
2021-12-07 21:14:06 +0800

    :bug: 修复问候插件无法设置数据库端口的BUG

--- core-mirai-ts/plugins/greeting/model/Base.js
+++ core-mirai-ts/plugins/greeting/model/Base.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql')
 const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.BOT_DATABASE
