039e36809343bb22e6bdf89da5c304805d29bfe3
retroxz <zzxee666@gmail.com>
2023-01-29 00:03:44 +0800

    :poop: 尝试将虚拟环境工具更换为poetry

--- plugins/bt/__init__.py
+++ plugins/bt/__init__.py
@@ -1,6 +1,6 @@
 from nonebot import on_command
 from asyncio.exceptions import TimeoutError
-from nonebot.typing import T_State
+
 
 
 
@@ -8,8 +8,19 @@ bt = on_command("bt")
 
 
 @bt.handle()
-async def _():
-    pass
+async def _(state,  arg):
+    msg = arg.extract_plain_text().strip().split()
+    if msg:
+        keyword = None
+        page = 1
+        if n := len(msg):
+            keyword = msg[0]
+        if n > 1 and is_number(msg[1]) and int(msg[1]) > 0:
+            page = int(msg[1])
+        state["keyword"] = keyword
+        state["page"] = page
+    else:
+        state["page"] = 1
 
 
 @bt.got("keyword", prompt="请输入要查询的内容！")
